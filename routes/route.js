e96c9f045fb8f8bc5c8ca3ef94f7825799e77cb6
Yelagandula Sai Teja <saiteja6734.yelagandula@gmail.com>
2023-03-19 18:42:49 +0530

    tyu

--- routes/route.js
+++ routes/route.js
@@ -18,7 +18,7 @@ router.post('/postquery',async(req,res)=>{
     const {name,email,organization,phone,siteloc} = req.body;
     console.log(req.body)
     const options = {
-      from:"COWE Customer Services",
+      from:"COWE Customer Services <saiteja6734.yelagandula@gmail.com>",
       to:email,
       subject:"Query from "+name,
       text:"Name : " + name + "\n" + "Email : "+email + "\n" + "Organization : "+organization + "\n" + "Phone Number : "  + phone + "\n" +  "Site Location : "+siteloc,
